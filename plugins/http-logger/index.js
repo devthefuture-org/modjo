2eaf88ab9fa57755c2688371448cecb508448cce
devthejo <jo@surikat.pro>
2022-06-13 18:16:36 +0200

    fix: http-logger hideSecrets

--- plugins/http-logger/index.js
+++ plugins/http-logger/index.js
@@ -4,15 +4,24 @@ const ctx = nctx.create(Symbol(__dirname.split("/").pop()))
 
 module.exports.create = () => {
   const logger = ctx.require("logger")
+  const config = ctx.require("config")
+  const { httpLogger: httpLoggerConfig = {} } = config
+  const { hideSecrets = [] } = httpLoggerConfig
   return (req, res, next) => {
     const originalSend = res.send
     res.send = function (...args) {
       originalSend.apply(res, args)
-      logger.info(`${req.method} ${req.url} ${res.statusCode}`)
+
+      let { url } = req
+      for (const secret of hideSecrets) {
+        url = url.replaceAll(secret, "*".repeat(secret.length))
+      }
+
+      logger.info(`${req.method} ${url} ${res.statusCode}`)
     }
     next()
   }
 }
-module.exports.dependencies = ["logger"]
+module.exports.dependencies = ["logger", "config"]
 
 module.exports.ctx = ctx
