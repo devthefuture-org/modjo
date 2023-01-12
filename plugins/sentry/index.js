b4c7ebc9fffdc37838be0af5e27692bf682f9e8a
devthejo <jo@surikat.pro>
2023-01-12 10:48:49 +0100

    feat: add sentry plugin

---
+++ plugins/sentry/index.js
@@ -0,0 +1,26 @@
+const nctx = require("nctx")
+
+const Sentry = require("@sentry/node")
+
+const ctx = nctx.create(Symbol(__dirname.split("/").pop()))
+
+module.exports.create = () => {
+  const options = ctx.require("config.sentry")
+
+  const { dsn = process.env.SENTRY_DSN } = options
+
+  if (dsn) {
+    return
+  }
+
+  Sentry.init({
+    dsn,
+    ...options,
+  })
+
+  return Sentry
+}
+
+module.exports.dependencies = ["config"]
+
+module.exports.ctx = ctx
