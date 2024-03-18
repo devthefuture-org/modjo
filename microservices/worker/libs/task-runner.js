4bf6df3b5dce00c205854c5f1aa68e765ee0d64d
devthejo <devthejo@protonmail.com>
2024-03-18 19:33:12 +0100

    fix: default factory for worker catch errors

--- microservices/worker/libs/task-runner.js
+++ microservices/worker/libs/task-runner.js
@@ -1,11 +1,30 @@
 const kebabCase = require("lodash.kebabcase")
 
+const { ctx } = require("../ctx")
+
+const defaultFactory = async function catchErrorFactory(handler) {
+  const logger = ctx.require("logger")
+  return async (...args) => {
+    try {
+      await handler(...args)
+    } catch (err) {
+      logger.error(err)
+    }
+  }
+}
+
 module.exports = async function createTaskRunner(q) {
   const queueHandlerFactories = require(`${process.cwd()}/build/queues`)
+  const { microserviceWorker: config = {} } = ctx.require("config")
 
   const queueFilename = kebabCase(q)
-  const factory = queueHandlerFactories[queueFilename]
-  const handler = await factory()
+  const userFactory = queueHandlerFactories[queueFilename]
+  let handler = await userFactory()
+
+  const factory = config.factory || defaultFactory
+  if (factory) {
+    handler = await factory(handler)
+  }
 
   return async function taskRunner(taskDefinition) {
     await handler(taskDefinition)
